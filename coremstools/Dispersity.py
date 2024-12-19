c7245d981ceb62ac4d55354b5a060248f5bf2ef9
Christian Dewey <dewey.christian@gmail.com>
2024-12-19 15:36:42 -0600

    dispersity writes to file instead of creating new file

--- coremstools/Dispersity.py
+++ coremstools/Dispersity.py
@@ -65,4 +65,4 @@ class Dispersity:
         mzs = list(assignments['m/z'].drop_duplicates())
         eics = parser.get_eics(target_mzs=mzs, tic_data={}, peak_detection=False, smooth=False)
         assignments['Dispersity'], assignments['Retention Time'] = zip(*assignments.apply(get_dispersity_rt, eics = eics, axis=1))
-        assignments.to_csv(sample.replace('.csv','_dispersity.csv'), index=False)
+        assignments.to_csv(sample.replace('.csv'), index=False)
